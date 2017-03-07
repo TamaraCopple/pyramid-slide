01158a8f50c661d941151be70a74fa3d727bdb83
Ceiligirl72 <ceiligirl72@gmail.com>
2017-03-06 21:46:50 -0600

    initial commit; html & CSS blocked in and mostly formatted. Need to integrate JS.

---
+++ pyramid-slide.js
@@ -0,0 +1,30 @@
+function drawPyramid(height) {
+
+    // first, clear the old content
+    document.getElementById("pyramid").innerHTML = "";
+
+    // for each row....
+    for (var row = 0;
+        row < height;
+        row++)
+        {
+        // figure out number of bricks and spaces
+        var numBricks = row + 2;
+        var numSpaces = height - row - 1;
+
+        // build up a string for this row
+        var rowStr = "";
+        for (var i = 0; i < numSpaces; i++) {
+            var spaceChar = "&nbsp"; // this is the HTML encoding for a space " "
+            rowStr += spaceChar;
+        }
+        for (var i = 0; i < numBricks; i++) {
+            rowStr += "#";
+        }
+
+        // make a <p> element for this row, and insert it into the #pyramid container
+        rowElem = document.createElement("p");
+        rowElem.innerHTML = rowStr;
+        document.getElementById("pyramid").appendChild(rowElem);
+    }
+};
